2ad7ec092da4a49fa18bd74378c03c1cf4546b01
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-04-21 21:44:42 -0400

    modularization

---
+++ resolvers.js
@@ -0,0 +1,48 @@
+const { persons } = require('./data')
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) => {
+        args.phone === 'YES' ? person.phone : !person.phone
+      }
+      return persons.filter(byPhone)
+    },
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      }
+    },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name,
+        })
+      }
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      if (!person) {
+        return null
+      }
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
+      return updatedPerson
+    },
+  },
+}
+
+module.exports = resolvers
